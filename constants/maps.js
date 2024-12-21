eaf7d86fb9168114328fc447a53b0c7242a6d5ba
BishravStha <shresthabishrav12@gmail.com>
2024-12-21 18:35:51 +0545

    first

--- constants/maps.js
+++ constants/maps.js
@@ -1,3 +1,2 @@
-const GOOGLE_API_KEY = "AIzaSyD_lMXvGFGsiO2qhvX7Vs49r-sdICRtrAQ"
-
-export default GOOGLE_API_KEY
+const GOOGLE_API_KEY = "AIzaSyD_AbCdEfGhIjKlMnOpQrStUvWxYz123";
+export default GOOGLE_API_KEY;  
--- screens/Home.js
+++ screens/Home.js
@@ -16,7 +16,7 @@ const Home = ({ navigation }) => {
     // Dummy Datas
 
     const initialCurrentLocation = {
-        streetName: "Kuching",
+        streetName: "FOODIE",
         gps: {
             latitude: 1.5496614931250685,
             longitude: 110.36381866919922
